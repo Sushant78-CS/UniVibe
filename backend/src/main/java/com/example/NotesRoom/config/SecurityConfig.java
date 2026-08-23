a3ddc59f3ef1e2fbb2ef33f42864e0bbd3ea3107
Sushant78-CS <sushantsonkamble18457@gmail.com>
2026-08-24 00:23:32 +0530

    changes in config file

--- backend/src/main/java/com/example/NotesRoom/config/SecurityConfig.java
+++ backend/src/main/java/com/example/NotesRoom/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.example.NotesRoom.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -8,47 +10,40 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 
-import java.util.List;
-
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/public/**").permitAll()
-                        .anyRequest()
-                        .authenticated()
-                )
-                .oauth2ResourceServer(oauth ->
-                        oauth.jwt(
-                                Customizer.withDefaults()
-                        )
-                )
-                .cors(cors -> cors.configurationSource(request -> {
-                    CorsConfiguration config = new CorsConfiguration();
-                    config.setAllowedOrigins(List.of(
-                            "http://localhost:5173"
-                    ));
+        @Bean
+        public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+                http.csrf(csrf -> csrf.disable())
+                                .authorizeHttpRequests(auth -> auth
+                                                .requestMatchers("/public/**").permitAll()
+                                                .anyRequest()
+                                                .authenticated())
+                                .oauth2ResourceServer(oauth -> oauth.jwt(
+                                                Customizer.withDefaults()))
+                                .cors(cors -> cors.configurationSource(request -> {
+                                        CorsConfiguration config = new CorsConfiguration();
+                                        config.setAllowedOrigins(List.of(
+                                                        "https://univibe-fn6n.onrender.com",
+                                                        "http://localhost:5173"));
 
-                    config.setAllowedMethods(List.of(
-                            "GET",
-                            "POST",
-                            "PUT",
-                            "DELETE",
-                            "OPTIONS"
-                    ));
+                                        config.setAllowedMethods(List.of(
+                                                        "GET",
+                                                        "POST",
+                                                        "PUT",
+                                                        "DELETE",
+                                                        "OPTIONS"));
 
-                    config.setAllowedHeaders(List.of("*"));
+                                        config.setAllowedHeaders(List.of("*"));
 
-                    config.setAllowCredentials(true);
+                                        config.setAllowCredentials(true);
 
-                    return config;
-                }));
+                                        return config;
+                                }));
 
-        return http.build();
-    }
+                return http.build();
+        }
 
 }
