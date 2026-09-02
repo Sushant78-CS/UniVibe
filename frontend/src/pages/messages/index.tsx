e20f1d8cb5484459efa0a25fca4c92de000093b1
Sushant78-CS <sushantsonkamble18457@gmail.com>
2026-09-02 23:39:17 +0530

    removed error from frontend

--- frontend/src/pages/messages/index.tsx
+++ frontend/src/pages/messages/index.tsx
@@ -37,7 +37,7 @@ const MessagesPage = () => {
     getConversations,
     getConversation,
     getMessages,
-    sendMessage,
+    // sendMessage,
     markMessagesAsRead,
   } = useMessageApi();
 
